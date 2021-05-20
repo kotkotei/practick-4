40f82f12c605b9752cab33b814c95e4536e85082
kotkotei <kotkotei@yandex.ru>
2021-05-20 18:41:25 +0700

    practick mega word puck

---
+++ Program  2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//Даны натуральные числа от 101 до 321. Напечатать те из них,которые делятся БЕЗ ОСТАТКА на 4, но не делятся на 6.
+            Console.WriteLine("Введите  число от 101");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите  число  321");
+            double b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine();
+
+            while (a <= b)
+            {
+                if (a % 4 == 0 && a % 6 != 0)
+                    Console.WriteLine(a);
+                a++;
+            }
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,538 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//ВАРИАНТ 10 Задание 1 Вводится число от 1 до 100.Дать название этого числа(1 - один, 2 - два, …, 100 - сто);
+            Console.WriteLine("Введи числ от 1 до 100");
+            double a = Convert.ToDouble(Console.ReadLine());
+            switch (a)
+            {
+                case 1:
+                    {
+                        Console.WriteLine("1 (Один)");
+                        break;
+                    }
+                case 2:
+                    {
+                        Console.WriteLine("2 (два)");
+                        break;
+                    }
+                case 3:
+                    {
+                        Console.WriteLine("3  (три)");
+                        break;
+                    }
+                case 4:
+                    {
+                        Console.WriteLine("4 (четыре)");
+                        break;
+                    }
+                case 5:
+                    {
+                        Console.WriteLine("5 (пять)");
+                        break;
+                    }
+                case 6:
+                    {
+                        Console.WriteLine("6 (шесть)");
+                        break;
+                    }
+                case 7:
+                    {
+                        Console.WriteLine("7 (семь)");
+                        break;
+                    }
+                case 8:
+                    {
+                        Console.WriteLine("8 (Восемь)");
+                        break;
+                    }
+                case 9:
+                    {
+                        Console.WriteLine("9 (девять)");
+                        break;
+                    }
+                case 10:
+                    {
+                        Console.WriteLine("10 (десять");
+                        break;
+                    }
+                case 11:
+                    {
+                        Console.WriteLine("11 (Одинадцать)");
+                        break;
+                    }
+                case 12:
+                    {
+                        Console.WriteLine("12 (двенадцать)");
+                        break;
+                    }
+                case 13:
+                    {
+                        Console.WriteLine("13  (тринадцать)");
+                        break;
+                    }
+                case 14:
+                    {
+                        Console.WriteLine("14 четырнадцать");
+                        break;
+                    }
+                case 15:
+                    {
+                        Console.WriteLine("15 (пятнадцать)");
+                        break;
+                    }
+                case 16:
+                    {
+                        Console.WriteLine("16 (шестнадцоть хей)");
+                        break;
+                    }
+                case 17:
+                    {
+                        Console.WriteLine("17 (семьнадцать)");
+                        break;
+                    }
+                case 18:
+                    {
+                        Console.WriteLine("18 (Восемьнадцать)");
+                        break;
+                    }
+                case 19:
+                    {
+                        Console.WriteLine("19 (девятьнадцать)");
+                        break;
+                    }
+                case 20:
+                    {
+                        Console.WriteLine("20 (двадцаоть");
+                        break;
+                    }
+                case 21:
+                    {
+                        Console.WriteLine("21 (двадцать один)");
+                        break;
+                    }
+                case 22:
+                    {
+                        Console.WriteLine("22 (двадцать два)");
+                        break;
+                    }
+                case 23:
+                    {
+                        Console.WriteLine("23  (двадцать три)");
+                        break;
+                    }
+                case 24:
+                    {
+                        Console.WriteLine("24 (двадцать четыре)");
+                        break;
+                    }
+                case 25:
+                    {
+                        Console.WriteLine("25 (двадцать пять)");
+                        break;
+                    }
+                case 26:
+                    {
+                        Console.WriteLine("26 (двадцать шесть)");
+                        break;
+                    }
+                case 27:
+                    {
+                        Console.WriteLine("27 (двадцать семь)");
+                        break;
+                    }
+                case 28:
+                    {
+                        Console.WriteLine("28 (двадцать Восемь)");
+                        break;
+                    }
+                case 29:
+                    {
+                        Console.WriteLine("29 (двадцать девять)");
+                        break;
+                    }
+                case 30:
+                    {
+                        Console.WriteLine("30 (тридцать одлин");
+                        break;
+                    }
+                case 31:
+                    {
+                        Console.WriteLine("31 (тридцатть Один)");
+                        break;
+                    }
+                case 32:
+                    {
+                        Console.WriteLine("32 (тридцать два)");
+                        break;
+                    }
+                case 33:
+                    {
+                        Console.WriteLine("33  (тридцать три)");
+                        break;
+                    }
+                case 34:
+                    {
+                        Console.WriteLine("34 (тридцать четыре)");
+                        break;
+                    }
+                case 35:
+                    {
+                        Console.WriteLine("35 (тидцать пять)");
+                        break;
+                    }
+                case 36:
+                    {
+                        Console.WriteLine("36 (тридцать шесть)");
+                        break;
+                    }
+                case 37:
+                    {
+                        Console.WriteLine("37 (тридацть семь)");
+                        break;
+                    }
+                case 38:
+                    {
+                        Console.WriteLine("38 (тридцать Восемь)");
+                        break;
+                    }
+                case 39:
+                    {
+                        Console.WriteLine("39 (тридцать девять)");
+                        break;
+                    }
+                case 40:
+                    {
+                        Console.WriteLine("40 (сорок");
+                        break;
+                    }
+                case 41:
+                    {
+                        Console.WriteLine("41 (сорок Один)");
+                        break;
+                    }
+                case 42:
+                    {
+                        Console.WriteLine("42 (сорок два)");
+                        break;
+                    }
+                case 43:
+                    {
+                        Console.WriteLine("43  (сорок три)");
+                        break;
+                    }
+                case 44:
+                    {
+                        Console.WriteLine("44 (сорок четыре)");
+                        break;
+                    }
+                case 45:
+                    {
+                        Console.WriteLine("45 (сроок пять)");
+                        break;
+                    }
+                case 46:
+                    {
+                        Console.WriteLine("46 (сорок шесть)");
+                        break;
+                    }
+                case 47:
+                    {
+                        Console.WriteLine("47 (сорок семь)");
+                        break;
+                    }
+                case 48:
+                    {
+                        Console.WriteLine("48 (сорок Восемь)");
+                        break;
+                    }
+                case 49:
+                    {
+                        Console.WriteLine("49 (сорок девять)");
+                        break;
+                    }
+                case 50:
+                    {
+                        Console.WriteLine("50 (пять десять");
+                        break;
+                    }
+                case 51:
+                    {
+                        Console.WriteLine("51 (пятьдесят Один)");
+                        break;
+                    }
+                case 52:
+                    {
+                        Console.WriteLine("52 (пятьдесят два)");
+                        break;
+                    }
+                case 53:
+                    {
+                        Console.WriteLine("53  (пятьдесят три)");
+                        break;
+                    }
+                case 54:
+                    {
+                        Console.WriteLine("54 пятьдесят четыре");
+                        break;
+                    }
+                case 55:
+                    {
+                        Console.WriteLine("55 (пятьдесят пять)");
+                        break;
+                    }
+                case 56:
+                    {
+                        Console.WriteLine("56 (пятьдесят шесть)");
+                        break;
+                    }
+                case 57:
+                    {
+                        Console.WriteLine("57 (пятьдесят семь)");
+                        break;
+                    }
+                case 58:
+                    {
+                        Console.WriteLine("58 (пятьдесят Восемь)");
+                        break;
+                    }
+                case 59:
+                    {
+                        Console.WriteLine("59 (пятьдесят девять)");
+                        break;
+                    }
+                case 60:
+                    {
+                        Console.WriteLine("60 (шестьдесять");
+                        break;
+                    }
+                case 61:
+                    {
+                        Console.WriteLine("61 (шейсят Один)");
+                        break;
+                    }
+                case 62:
+                    {
+                        Console.WriteLine("62 (шейсят два)");
+                        break;
+                    }
+                case 63:
+                    {
+                        Console.WriteLine("63  (шейсят три)");
+                        break;
+                    }
+                case 64:
+                    {
+                        Console.WriteLine("64 (шейсят четыре)");
+                        break;
+                    }
+                case 65:
+                    {
+                        Console.WriteLine("65 (шейсят пять)");
+                        break;
+                    }
+                case 66:
+                    {
+                        Console.WriteLine("66 (шейсят шесть)");
+                        break;
+                    }
+                case 67:
+                    {
+                        Console.WriteLine("67 (шейсят семь)");
+                        break;
+                    }
+                case 68:
+                    {
+                        Console.WriteLine("68 (шейсятсВосемь)");
+                        break;
+                    }
+                case 69:
+                    {
+                        Console.WriteLine("69 (шейсят девять)");
+                        break;
+                    }
+                case 70:
+                    {
+                        Console.WriteLine("70 (семьдесят");
+                        break;
+                    }
+                case 71:
+                    {
+                        Console.WriteLine("71 (семьдесят Один)");
+                        break;
+                    }
+                case 72:
+                    {
+                        Console.WriteLine("72 (семьдесят два)");
+                        break;
+                    }
+                case 73:
+                    {
+                        Console.WriteLine("73  (семьдесят три)");
+                        break;
+                    }
+                case 74:
+                    {
+                        Console.WriteLine("74 (семьдесят четыре)");
+                        break;
+                    }
+                case 75:
+                    {
+                        Console.WriteLine("75 (семьдесят пять)");
+                        break;
+                    }
+                case 76:
+                    {
+                        Console.WriteLine("76 (семьдесят шесть)");
+                        break;
+                    }
+                case 77:
+                    {
+                        Console.WriteLine("77 (семьдесят семь)");
+                        break;
+                    }
+                case 78:
+                    {
+                        Console.WriteLine("78 (семьдесят Восемь)");
+                        break;
+                    }
+                case 79:
+                    {
+                        Console.WriteLine("79 (семьдесят девять)");
+                        break;
+                    }
+                case 80:
+                    {
+                        Console.WriteLine("80 (семьдесят");
+                        break;
+                    }
+                case 81:
+                    {
+                        Console.WriteLine("81 (Восемьдесят  Один)");
+                        break;
+                    }
+                case 82:
+                    {
+                        Console.WriteLine("82 (Восемьдесят два)");
+                        break;
+                    }
+                case 83:
+                    {
+                        Console.WriteLine("83  (Восемьдесят три)");
+                        break;
+                    }
+                case 84:
+                    {
+                        Console.WriteLine("84 (Восемьдесят четыре)");
+                        break;
+                    }
+                case 85:
+                    {
+                        Console.WriteLine("85 (Восемьдесят пять)");
+                        break;
+                    }
+                case 86:
+                    {
+                        Console.WriteLine("86 (Восемьдесят шесть)");
+                        break;
+                    }
+                case 87:
+                    {
+                        Console.WriteLine("87 (Восемьдесят семь)");
+                        break;
+                    }
+                case 88:
+                    {
+                        Console.WriteLine("88 (Восемьдесят Восемь)");
+                        break;
+                    }
+                case 89:
+                    {
+                        Console.WriteLine("89 (Восемьдесят девять)");
+                        break;
+                    }
+                case 90:
+                    {
+                        Console.WriteLine("90 (девяносто");
+                        break;
+                    }
+                case 91:
+                    {
+                        Console.WriteLine("91 (девяносто Один)");
+                        break;
+                    }
+                case 92:
+                    {
+                        Console.WriteLine("92 (девяносто два)");
+                        break;
+                    }
+                case 93:
+                    {
+                        Console.WriteLine("93  (девяносто три)");
+                        break;
+                    }
+                case 94:
+                    {
+                        Console.WriteLine("94 (девяносто четыре)");
+                        break;
+                    }
+                case 95:
+                    {
+                        Console.WriteLine("95 (девяносто пять)");
+                        break;
+                    }
+                case 96:
+                    {
+                        Console.WriteLine("6 (шесть)");
+                        break;
+                    }
+                case 97:
+                    {
+                        Console.WriteLine("97 (девяносто семь)");
+                        break;
+                    }
+                case 98:
+                    {
+                        Console.WriteLine("98 (девяносто Восемь)");
+                        break;
+                    }
+                case 99:
+                    {
+                        Console.WriteLine("99 (девять)");
+                        break;
+                    }
+                case 100:
+                    {
+                        Console.WriteLine("100 (Дождикне морозь мне под сто)");
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("дождик дождиене морозь я туска");
+                        break;
+                    }
+            }
+                    Console.ReadKey();
+
+
+
+
+
+
+
+
+
+            }
+            }
+
+        }
+    
+
